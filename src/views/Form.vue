a0ffd731383318c2bca461cac159d7b284bf8308
Wang Chao <wangchao2423@gmail.com>
2025-02-23 13:17:53 +0800

    样式

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -3,7 +3,7 @@
  * @Author     : itchaox
  * @Date       : 2023-09-26 15:10
  * @LastAuthor : Wang Chao
- * @LastTime   : 2025-02-23 13:08
+ * @LastTime   : 2025-02-23 13:16
  * @desc       : Markdown 预览插件
 -->
 <script setup>
@@ -531,6 +531,8 @@
           <el-button
             size="small"
             @click="copyContent"
+            type="primary"
+            plain
             class="copy-button"
           >
             <el-icon style="margin-right: 4px"><DocumentCopy /></el-icon>
