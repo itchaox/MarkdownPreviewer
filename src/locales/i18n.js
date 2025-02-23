3fe7c0c738f978d2f4d94c01ff02ba7649401ac2
Wang Chao <wangchao2423@gmail.com>
2025-02-23 08:17:46 +0800

    日文国际化

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -1,20 +1,19 @@
-import { createI18n } from 'vue-i18n'
+import { createI18n } from 'vue-i18n';
 import en from './en.json';
 import zh from './zh.json';
-import { bitable } from '@lark-base-open/js-sdk'
-
-
+import ja from './ja.json';
+import { bitable } from '@lark-base-open/js-sdk';
 
 export const i18n = createI18n({
   locale: 'en',
   allowComposition: true, // 占位符支持
   messages: {
     en: en,
-    zh: zh
-  }
-})
+    zh: zh,
+    ja: ja,
+  },
+});
 
 bitable.bridge.getLanguage().then((lang) => {
-  i18n.global.locale = lang
-})
-
+  i18n.global.locale = lang;
+});
