60cc942ed1b7afd4b5704bbe0c96b73f8029e1e0
Wang Chao <wangchao2423@gmail.com>
2025-02-24 17:03:13 +0800

    样式

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -3,7 +3,7 @@
  * @Author     : itchaox
  * @Date       : 2023-09-26 15:10
  * @LastAuthor : Wang Chao
- * @LastTime   : 2025-02-24 16:54
+ * @LastTime   : 2025-02-24 17:02
  * @desc       : Markdown 预览插件
 -->
 <script setup>
@@ -684,8 +684,8 @@
     color: #909399;
 
     img {
-      width: 180px;
-      height: 180px;
+      width: 175px;
+      height: 185px;
 
       &:first-child {
         margin-right: 30px;
