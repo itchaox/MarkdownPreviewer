7a33db2160254294503aea077c8742192e7dfc74
Wang Chao <wangchao2423@gmail.com>
2025-03-06 09:49:08 +0800

    编辑-实时更新问题

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -3,7 +3,7 @@
  * @Author     : itchaox
  * @Date       : 2023-09-26 15:10
  * @LastAuthor : Wang Chao
- * @LastTime   : 2025-03-06 07:30
+ * @LastTime   : 2025-03-06 09:48
  * @desc       : Markdown 预览插件
 -->
 <script setup>
@@ -118,7 +118,8 @@
     // 更新多维表格中的值
     try {
       const table = await bitable.base.getActiveTable();
-      await table.setCellValue(currentFieldId.value, recordId.value, [{ type: 'text', text: currentValue.value }]);
+      const field = await table.getField(lastSelectedFieldId.value);
+      await field.setValue(lastSelectedRecordId.value, currentValue.value);
     } catch (error) {
       console.error('更新单元格失败:', error);
       ElMessage.error('更新失败');
